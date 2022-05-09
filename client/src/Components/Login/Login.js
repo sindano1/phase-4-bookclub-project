48b06196252eaddf2990854376e0b0838d7b789a
ybuchi <yabuch_ch@hotmail.com>
2022-05-09 14:28:34 -0400

    Did some changes to the login form

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
@@ -7,6 +7,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
 function Login(){
+
     return(
         <article id="login-card">
             <header>
@@ -17,6 +18,7 @@ function Login(){
                 <header>
                     <h3>Login.</h3>
                 </header>
+                <Container>
                 <Form style={{textAlign: "left"}}>
                     <Form.Group>
                         <Form.Label>Username:</Form.Label>
@@ -28,6 +30,7 @@ function Login(){
                     </Form.Group>
                     <Button type="submit">Login</Button>
                 </Form>
+                </Container>
             </section>
             <hr/>
             <h4>OR</h4>
@@ -36,7 +39,23 @@ function Login(){
                 <header>
                     <h3>Create a new account.</h3>
                 </header>
+                <Container>
                 <Form style={{textAlign: "left"}}>
+                        <Row>
+                            <Col xs={6}>
+                                <Form.Group>
+                                    <Form.Label>First Name:</Form.Label>
+                                    <Form.Control type="text" placeholder="first name"></Form.Control>
+                                </Form.Group>
+                            </Col>
+                
+                            <Col xs={6}>
+                                <Form.Group>
+                                    <Form.Label>Last Name:</Form.Label>
+                                    <Form.Control type="text" placeholder="username"></Form.Control>
+                                </Form.Group>
+                            </Col>
+                        </Row>
                     <Form.Group>
                         <Form.Label>Username:</Form.Label>
                         <Form.Control type="text" placeholder="username"></Form.Control>
@@ -52,6 +71,7 @@ function Login(){
                     </Form.Group>
                     <Button type="submit">Create account</Button>
                 </Form>
+                </Container>
             </section>
         </article>
     )
